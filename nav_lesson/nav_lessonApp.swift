6c8d6cde931077f6ee24b2a6e966b7093e5e8bc7
traceybullington <traceylbullington@gmail.com>
2025-07-09 21:33:52 -0500

    Initial Commit

---
+++ nav_lesson/nav_lessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  nav_lessonApp.swift
+//  nav_lesson
+//
+//  Created by Tracey Bullington on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct nav_lessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
